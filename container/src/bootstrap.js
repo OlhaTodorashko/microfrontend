1ed6a0a57fb9c6430ca4ce131ca618686a6f72c2
olha todorashko <iolegrai@gmail.com>
2025-03-18 17:15:43 +0100

    S3L27: Refactoring Cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
 import { mount as productsMount } from 'products/ProductsIndex'; // './ProductsIndex': './src/index'
-import 'cart/CartShow'; // './CartShow': './src/index'
+import { mount as cartMount } from 'cart/CartShow'; // './CartShow': './src/index'
 
 productsMount(document.querySelector('#dev-products-host'));
+cartMount(document.querySelector('#dev-cart-host'));
 
 console.log('Container');
